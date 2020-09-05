1ca3e78580e28e5c4b0954c43be6e662fd37f266
Larsen Close <larsenclose@protonmail.com>
2020-09-05 14:50:21 -0600

    starting settings

--- Box.java
+++ Box.java
@@ -1,9 +1,31 @@
-
-
-
-
-
+import java.nio.FloatBuffer;
 
 public class Box {
-    
+
+    // statics set from settings class
+    private float speed = Settings.speed;
+    private float bullets = Settings.initial_bullets;
+
+
+    private float x; 
+    private float y;
+    private Float angle;
+
+
+    public Box(Triangle original) {
+        Triangle origin = original;
+        new Triangle()
+
+       this.x = x;
+       this.y = y;
+
+    }
+ 
+    // put all the data for this triangle to
+    // pb and cb
+    public void sendData( FloatBuffer pb) {
+       this.speed.sendData( pb);
+
+    }
+
 }
